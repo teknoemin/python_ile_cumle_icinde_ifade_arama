2029cbcf38ad0b0c1d15cc844ef72cd16c53a682
teknoemin <teknoemin@gmail.com>
2020-12-11 00:45:33 +0300

    Add files via upload

---
+++ soru3.py
@@ -0,0 +1,9 @@
+﻿ana_metin = "USAK emin"
+aranacak_obek = "ver"
+arama_komutu = ana_metin.find(aranacak_obek)
+if (arama_komutu > 0):
+	onceki = ana_metin[arama_komutu - 1]
+	sonraki = ana_metin[arama_komutu + len(aranacak_obek)]
+	print(onceki + aranacak_obek + sonraki)
+else:
+	print("Aranan değer bulunamadı!")
